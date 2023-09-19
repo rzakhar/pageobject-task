6101b445ca63bf98f42abb758b39f3eab15c5e34
Roman Zakharov <romanzakharov329@gmail.com>
2023-09-19 16:12:30 +0300

    Add dots

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 import pytest
 
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
-from pages.basket_page import BasketPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.login_guest
--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,9 @@ import time
 
 import pytest
 
-from pages.product_page import ProductPage
-from pages.login_page import LoginPage
-from pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
