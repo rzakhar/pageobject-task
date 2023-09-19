7a910e358ebfb1c9e92a2c4bcc63b200ede1df23
Roman Zakharov <romanzakharov329@gmail.com>
2023-09-19 10:43:41 +0300

    Update the test

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url.contains("login"), "Should be on a login page"
+        assert "login" in self.browser.current_url, "Should be on a login page"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form should be presented"
