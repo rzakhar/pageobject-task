0c20a4a800c2d603cd2a315bddb6d5bb47022690
Roman Zakharov <romanzakharov329@gmail.com>
2023-09-19 13:22:51 +0300

    Add two new login tests for the product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,6 +7,7 @@ import math
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -55,3 +56,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
